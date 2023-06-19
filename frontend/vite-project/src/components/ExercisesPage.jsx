d5fce36cb79f0280f80e66b10c23841eb81d3808
tbroder14 <111935914+tbroder14@users.noreply.github.com>
2023-06-19 18:48:46 -0500

    wp

--- frontend/vite-project/src/components/ExercisesPage.jsx
+++ frontend/vite-project/src/components/ExercisesPage.jsx
@@ -1,8 +1,8 @@
 export default function Exercises() {
     return(
         <> 
-            <div className="flex text-6xl text-center mt-5 mb-3 tracking-wide justify-center items-center">WE'RE PREGNANT!</div>
-                
+            <div className="flex text-6xl text-center mt-5 mb-3 tracking-wide justify-center items-center">Exercise Page</div>
+
         </>
     )
 
